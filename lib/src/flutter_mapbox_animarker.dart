0cfa7b4ecf92f9a16c21ec069c28cc1ac3f81ec1
Michael Aidoo <aidoomichael20@gmail.com>
2024-09-16 13:05:04 +0000

    minor changes

--- lib/src/flutter_mapbox_animarker.dart
+++ lib/src/flutter_mapbox_animarker.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:typed_data';
-import 'package:flutter/animation.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'utils/common.dart';
@@ -25,14 +24,7 @@ class MarkerAnimator extends TickerProvider {
 
   late Ticker _ticker;
 
-  /// A map storing animation controllers for markers.
-  final Map<String, AnimationController> _controller = {};
-
-  /// A map storing position animations for markers.
-  final Map<String, Animation<Offset>> _positionAnimation = {};
-
-  /// A map storing rotation animations for markers.
-  final Map<String, Animation<double>> _rotationAnimation = {};
+  final double _interval = 0.02; // Update interval in seconds
 
   @override
   Ticker createTicker(TickerCallback onTick) {
@@ -50,14 +42,10 @@ class MarkerAnimator extends TickerProvider {
   /// - [data]: A list of position and rotation data for the marker animation {position: [double, double], rotation: double}[].
   /// - [properties]: (Optional) Custom properties for marker style.
   /// - [scale]: (optional) A scale factor for the image.
-  /// - [animationDuration]: (optional) How long the animation should last.
-  /// - [positionCurve]: (optional) Custom curve for position animations.
-  /// - [rotationCurve]: (optional) Custom curve for rotation animations.
   Future<void> addMarkerPoint(
     String markerId,
     String markerImage,
     List<Map<String, dynamic>> data, {
-    int animationDuration = 50,
     double scale = 15,
     Map<String, dynamic> properties = const {
       "icon-opacity": 1.0,
@@ -67,30 +55,23 @@ class MarkerAnimator extends TickerProvider {
       'icon-allow-overlap': true,
       'icon-ignore-placement': true,
     },
-    Curve positionCurve = Curves.linear,
-    Curve rotationCurve = Curves.linear,
   }) async {
     try {
       // Store marker points (positions and rotations).
       markerPoints.putIfAbsent(markerId, () => []);
       for (var data in data) {
-        Point newPoint = Point(
-            coordinates: Position(data['position'][0], data['position'][1]));
+        Point newPoint = Point(coordinates: Position(data['position'][0], data['position'][1]));
         num rotation = data['rotation'];
         markerPoints[markerId]?.add({'point': newPoint, 'rotation': rotation});
       }
 
       // Initialize the marker stream if it doesn't exist.
-      if (!_locationStreamControllers.containsKey(markerId) &&
-          markerPoints[markerId]!.length > 2) {
+      if (!_locationStreamControllers.containsKey(markerId) && markerPoints[markerId]!.length > 2) {
         await _initializeMarkerStreams(
           markerId: markerId,
           markerImage: markerImage,
           properties: properties,
           scale: scale,
-          animationDuration: animationDuration,
-          positionCurve: positionCurve,
-          rotationCurve: rotationCurve,
         );
       }
 
@@ -119,9 +100,7 @@ class MarkerAnimator extends TickerProvider {
 
         // Update the last layer ID if necessary.
         if (_lastLayerId == layerId) {
-          _lastLayerId = markerLayerIds.values.isNotEmpty
-              ? markerLayerIds.values.last
-              : null;
+          _lastLayerId = markerLayerIds.values.isNotEmpty ? markerLayerIds.values.last : null;
         }
       }
     } on Exception catch (_) {
@@ -134,10 +113,10 @@ class MarkerAnimator extends TickerProvider {
     required String markerId,
     required String markerImage,
     required double scale,
-    required int animationDuration,
+    // required int animationDuration,
     required Map<String, dynamic> properties,
-    required Curve positionCurve,
-    required Curve rotationCurve,
+    // required Curve positionCurve,
+    // required Curve rotationCurve,
   }) async {
     try {
       List<Map<String, dynamic>>? points = markerPoints[markerId];
@@ -182,19 +161,10 @@ class MarkerAnimator extends TickerProvider {
           return;
         }
 
-        await mapboxMap.style.addStyleImage(
-            iconId,
-            scale,
-            MbxImage(width: width, height: height, data: imgU8List),
-            false,
-            [],
-            [],
-            null);
+        await mapboxMap.style.addStyleImage(iconId, scale, MbxImage(width: width, height: height, data: imgU8List), false, [], [], null);
         await mapboxMap.style.addStyleSource(sourceId, json.encode(source));
         LayerPosition layerPosition = LayerPosition(below: _lastLayerId);
-        await mapboxMap.style.addStyleLayer(
-            json.encode({"id": layerId, "type": "symbol", "source": sourceId}),
-            layerPosition);
+        await mapboxMap.style.addStyleLayer(json.encode({"id": layerId, "type": "symbol", "source": sourceId}), layerPosition);
         await mapboxMap.style.setStyleLayerProperties(
           layerId,
           json.encode(
@@ -219,8 +189,7 @@ class MarkerAnimator extends TickerProvider {
       _locationStreamControllers[markerId] = controller;
       _locationStreamControllers[markerId]?.stream.asyncMap((_) async {
         if (markerPoints[markerId]!.length > 1) {
-          await _animateMarkerWithAnimation(
-              markerId, animationDuration, positionCurve, rotationCurve);
+          await _animateVehicle(markerId);
         }
       }).listen((_) {});
     } on Exception catch (_) {
@@ -228,83 +197,36 @@ class MarkerAnimator extends TickerProvider {
     }
   }
 
-  Future<void> _animateMarkerWithAnimation(
-    String markerId,
-    int animationDuration,
-    Curve positionCurve,
-    Curve rotationCurve,
-  ) async {
+  Future<void> _animateVehicle(String driverId) async {
     try {
-      List<Map<String, dynamic>> points = markerPoints[markerId]!;
-
-      String iconId = 'icon-$markerId';
-      String layerId = 'layer-$markerId';
-      String sourceId = 'source-$markerId';
-
-      // Inside your animation loop, adjust the rotation tween
+      List<Map<String, dynamic>> points = markerPoints[driverId]!;
+      String iconId = 'icon-$driverId';
+      String layerId = 'layer-$driverId';
+      String sourceId = 'source-$driverId';
       for (int i = 0; i < points.length - 1; i++) {
-        Point startPoint = points[i]['point'];
-        Point endPoint = points[i + 1]['point'];
-        Duration duration = Duration(milliseconds: animationDuration);
-        // Create AnimationController and Tween for position and rotation
-        _controller[markerId] =
-            AnimationController(duration: duration, vsync: this);
+        Point point = points[i]['point'];
+        // double targetRotation = points[i]['rotation'].toDouble();
         double startRotation = points[i]['rotation'].toDouble();
         double endRotation = points[i + 1]['rotation'].toDouble();
 
         // Normalize the end rotation to avoid jumps
         endRotation = normalizeRotation(startRotation, endRotation);
-
-        _positionAnimation[markerId] = Tween<Offset>(
-          begin: Offset(startPoint.coordinates[0]!.toDouble(),
-              startPoint.coordinates[1]!.toDouble()),
-          end: Offset(endPoint.coordinates[0]!.toDouble(),
-              endPoint.coordinates[1]!.toDouble()),
-        ).animate(
-          _controller[markerId]!.drive(CurveTween(curve: positionCurve)),
-        );
-
-        _rotationAnimation[markerId] = Tween<double>(
-          begin: startRotation,
-          end: endRotation,
-        ).animate(
-          _controller[markerId]!.drive(CurveTween(curve: rotationCurve)),
-        );
-
-        // Add a listener to the animation to update the marker position and rotation
-        _controller[markerId]!.addListener(() async {
-          var source = {
-            "type": "geojson",
-            "data": {
-              "type": "Feature",
-              "geometry": {
-                "type": "Point",
-                "coordinates": [
-                  _positionAnimation[markerId]?.value.dx,
-                  _positionAnimation[markerId]?.value.dy
-                ]
-              }
+        var source = {
+          "type": "geojson",
+          "data": {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [point.coordinates[0], point.coordinates[1]]
             }
-          };
-
-          var iconProperties = {
-            "icon-image": iconId,
-            "icon-rotate": _rotationAnimation[markerId]?.value
-          };
-
-          // Update the marker's position and rotation on the map
-          await mapboxMap.style
-              .setStyleSourceProperties(sourceId, json.encode(source));
-          await mapboxMap.style
-              .setStyleLayerProperties(layerId, json.encode(iconProperties));
-        });
-
-        // Start the animation
-        await _controller[markerId]?.forward();
-        _controller[markerId]
-            ?.dispose(); // Dispose the controller after each iteration
+          }
+        };
+        var iconProperties = {"icon-image": iconId, "icon-rotate": endRotation.toDouble()};
+        await mapboxMap.style.setStyleSourceProperties(sourceId, json.encode(source));
+        await mapboxMap.style.setStyleLayerProperties(layerId, json.encode(iconProperties));
+        await Future.delayed(Duration(milliseconds: (_interval * 500).toInt()));
       }
-      markerPoints[markerId]?.clear();
+      markerPoints[driverId]!.clear();
     } on Exception catch (_) {
       rethrow;
     }
